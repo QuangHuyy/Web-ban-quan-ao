2a1d78a763716eb46f7521e0d00cd1da91ed6978
linhtunguyen <linhcva31399@gmail.com>
2020-06-25 16:05:58 +0700

    sửa kích cỡ ảnh của trang giỏ hàng, kích cỡ nút

--- CNW_WebBanQuanAo/Controllers/CartController.cs
+++ CNW_WebBanQuanAo/Controllers/CartController.cs
@@ -64,7 +64,7 @@ namespace CNW_WebBanQuanAo.Controllers
             context.GIOHANG.Add(model);
             context.SaveChanges();
 
-            return Redirect("https://localhost:44332/Home/Index");
+            return Redirect("https://Home/Index");
 
 
         }
@@ -117,7 +117,7 @@ namespace CNW_WebBanQuanAo.Controllers
                 }
                 return View(cart);
             }
-            return Redirect("https://localhost:44332/Account/DangNhap");
+            return Redirect("https://Account/DangNhap");
         }
 
 
@@ -172,7 +172,7 @@ namespace CNW_WebBanQuanAo.Controllers
 
                     cart.Clear();
                     Session["CartSession"] = cart;
-                    return Redirect("https://localhost:44332/Home/Index");
+                    return Redirect("https://Home/Index");
 
                 }
 
@@ -184,7 +184,7 @@ namespace CNW_WebBanQuanAo.Controllers
             }
             else
             {
-                return Redirect("https://localhost:44332/Account/DangNhap");
+                return Redirect("https://Account/DangNhap");
             }
 
 
@@ -346,7 +346,7 @@ namespace CNW_WebBanQuanAo.Controllers
 
 
             
-            return Redirect("https://localhost:44332/Home/Index");
+            return Redirect("https://Home/Index");
 
         }
 
